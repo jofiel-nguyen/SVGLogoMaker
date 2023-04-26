c65d5e9d76d45ded6a3b5ebda97894589b39b02a
wren <zennguyen2511@gmail.com>
2023-04-25 20:05:16 -0500

    update code for color

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ inquirer
   .then(answers => {
     const svgCode = `
       <svg viewBox="0 0 300 200" xmlns="http://www.w3.org/2000/svg">
-        <rect width="100%" height="100%" fill="${answers.shapeColor}" />
-        <${answers.shape} cx="150" cy="100" r="80" fill="none" stroke="${answers.textColor}" stroke-width="5" />
+        <rect width="100%" height="100%" fill="${answers.textColor}" />
+        <${answers.shape} cx="150" cy="100" r="80" fill="${answers.shapeColor}" stroke="${answers.textColor}" stroke-width="5" />
         <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="60" fill="${answers.textColor}">${answers.text}</text>
       </svg>
     `;
